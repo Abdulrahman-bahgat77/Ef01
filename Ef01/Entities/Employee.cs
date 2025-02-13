50806ada571ca069d955470303c293534ed59487
abdulrahman <abdulrahman403868@fci.bu.edu.eg>
2025-02-13 14:33:43 +0200

    Add project files.

---
+++ Ef01/Entities/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ef01.Entities
+{
+    internal class Employee
+    {
+        public int Id { get; set; } //Pk [identity contain(1,1)]
+
+        public string?  Name { get; set; } //nullable<string> Allow null
+                                           //nvarchar(max)
+
+        public decimal Salary { get; set; } //Decimal(18,12)
+
+        public int? Age { get; set; }  //Int
+    }
+}
---
+++ Ef01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ef01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
